Linear regression parameter regularization; fixed returning type of gradient method

model.py
@@ -43,6 +43,7 @@ class LinearRegressor:
     #   x - 2D array with shape (m, n), where m - number of training examples, n - number of features
     #   y - 1D array of targets with shape of m
     #   a - learning rate
+    #   lamb - regularization parameter
     #   iterations - number of iterations over the whole training set
     #   debug - if True, function returns learning history. If False - converging cost
     # Returns:
@@ -52,15 +53,16 @@ class LinearRegressor:
             x: np.ndarray,
             y: np.ndarray,
             a: float,
+            lamb: float,
             iterations: int,
             debug=False
             ) -> list[LearningHistPoint] | float:
         if not isfinite(x, y, a, iterations):
             raise ValueError('Infinite and NaN values are not allowed')
         if debug:
-            return self._debugging_fit(x, y, a, iterations)
+            return self._debugging_fit(x, y, a, lamb, iterations)
         else:
-            return self._fit(x, y, a, iterations)
+            return self._fit(x, y, a, lamb, iterations)
 
     # Parameters:
     #   x - 2D array with shape (m, n), where m - number of training examples, n - number of features
@@ -71,33 +73,34 @@ class LinearRegressor:
 
     # Parameters:
     #   x - 2D array with shape (m, n), where m - number of training examples, n - number of features
+    #   lamb - regularization parameter
     #   y - 1D array of targets with shape of m
     # Returns:
     #   float - cost of model for x.
-    def cost(self, x: np.ndarray, y: np.ndarray) -> float:
-        return np.mean(self._loss(x, y))
+    def cost(self, x: np.ndarray, y: np.ndarray, lamb: float) -> float:
+        return np.mean(self._loss(x, y)) + (lamb / 2 * x.shape[0]) * np.sum(self._w ** 2)
 
     def _loss(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
         return (self.predict(x) - y) ** 2 / 2
 
-    def _gradient(self, x: np.ndarray, y: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    def _gradient(self, x: np.ndarray, y: np.ndarray, lamb: float) -> tuple[np.ndarray, float]:
         error = self.predict(x) - y
-        dj_dw = (error @ x) / x.shape[0]
+        dj_dw = (error @ x + lamb * self._w) / x.shape[0]
         dj_db = np.mean(error)
         return dj_dw, dj_db
 
-    def _fit(self, x, y, a, iterations) -> float:
+    def _fit(self, x, y, a, lamb, iterations) -> float:
         for i in range(iterations):
-            dj_dw, dj_db = self._gradient(x, y)
+            dj_dw, dj_db = self._gradient(x, y, lamb)
             self._w -= a * dj_dw
             self._b -= a * dj_db
-        return self.cost(x, y)
+        return self.cost(x, y, lamb)
 
-    def _debugging_fit(self, x, y, a, iterations) -> list[LearningHistPoint]:
+    def _debugging_fit(self, x, y, a, lamb, iterations) -> list[LearningHistPoint]:
         hist = []
         for i in range(iterations):
-            dj_dw, dj_db = self._gradient(x, y)
+            dj_dw, dj_db = self._gradient(x, y, lamb)
             self._w -= a * dj_dw
             self._b -= a * dj_db
-            hist.append(LearningHistPoint(cost=self.cost(x, y), dj_dw=dj_dw, dj_db=dj_db, w=self._w, b=self._b))
+            hist.append(LearningHistPoint(cost=self.cost(x, y, lamb), dj_dw=dj_dw, dj_db=dj_db, w=self._w, b=self._b))
         return hist